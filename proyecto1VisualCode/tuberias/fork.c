8972352d777c5d4290c3da40a907b3a454115ea6
Francisco Algaba <27973@iessierradeguara.com>
2018-10-19 10:31:32 +0200

    Añade el archivo fork.c
    Este cambio añade el archivo fork.c
    en el que se explican las tareas
    multiproceso (forks). Es el primer
    programa con forks.

--- proyecto1VisualCode/tuberias/fork.c
+++ proyecto1VisualCode/tuberias/fork.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
-    printf("Hola 1\n");
-    fork();
-    printf("Hola 2\n");
-    fflush(stdout);
-    sleep(1);
-    return 0;
+    int a = 100;
+    pid_t pid = fork();
+    if (pid == -1) {
+        fprintf(stderr, "Error en el fork...\n");
+        exit(-1);
+    }
+    else if (pid == 0) { // Código del hijo
+        printf("Hola soy el hijo y mi pid es: %d y el de mi padre es: %d\n", getpid(), getppid());
+        sleep(2);
+        pid_t pidNieto;
+        if (pidNieto = fork() == -1) {
+            exit(-1);
+        }
+        else if (pidNieto == 0) { // Soy el nieto
+            printf("Hola soy el nieto y mi pid es: %d y el de mi padre es: %d\n", getpid(), getppid());
+        }
+        else { // Soy el hijo
+            wait(NULL);
+            fprintf(stdout, "El nieto ha acabado...\n");  
+        }
+        printf("Soy un proceso mi pid es: %d\n", getpid()); // Lo imprime el hijo y el nieto
+    }
+    else { // Código del padre
+        fprintf(stdout, "Soy el padre ");
+        fprintf(stdout, "y mi pid es: %d %d. El de mi hijo es: %d\n", getpid(), --a, pid);
+        wait(NULL); // Espera a que termine el hijo.
+        fprintf(stdout, "El hijo ha acabado...\n");
+        return 0;
+    }
 }
